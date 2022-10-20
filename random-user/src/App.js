44795caeca89e14dabba98b6cbb7b4d3d63394ef
pnichols789 <pnichols789@gmail.com>
2022-10-20 12:11:39 -0500

    -

--- random-user/src/App.js
+++ random-user/src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import User from "./components/User";
 
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Header/>
       <User/>
+      <Footer/>
     </div>
   );
 }
