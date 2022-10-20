5d3ac8efaad61695992701de25547c3035fc1f3b
pnichols789 <pnichols789@gmail.com>
2022-10-20 12:09:06 -0500

    -

--- random-user/src/App.js
+++ random-user/src/App.js
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
+import User from "./components/User";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Header/>
+      <User/>
     </div>
   );
 }
